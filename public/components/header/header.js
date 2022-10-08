c7967faca250340211af72b07557cb545db3d8a7
Студенческая <@rbiu.local>
2022-10-08 12:57:13 +0500

    header added

---
+++ public/components/header/header.js
@@ -0,0 +1,52 @@
+import './header'
+import './header.css'
+
+function Header(){
+    return(
+        <div>
+          
+        <form action="" method="get">
+        
+            
+        </form>
+        
+        
+
+        <input placeholder="Фильм, событие, время">
+            
+        </input>
+        
+        
+        <a href="">
+        <button type="submit"></button>
+        </a>
+        
+        <a href="">
+          <button1 type="submit" value="Кнопка"></button1> 
+        </a>
+        
+        
+        <a href="">
+          <button2 type="submit" value="Logo"></button2>
+        </a>
+        <form2 action="" method="get">
+        
+            
+        </form2>
+
+        <a href="">
+        <button3 type="submit" value="Все"></button3>
+        </a>
+        <a href="">
+        <button4 type="submit" value="КИНО"></button4>
+        </a>
+        <a href="">
+        <button5 type="submit" value="ТЕАТР"></button5>
+        </a>
+        <a href="">
+        <button6 type="submit" value="ВЫСТАВКИ"></button6>
+        </a>
+        </div>
+    )   
+}
+export default Header;
