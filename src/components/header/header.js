3fcf36264b48b85dcc93f6c05c1d87f3d4993934
Студенческая <@rbiu.local>
2022-10-08 14:26:05 +0500

    header fixed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,52 +1,52 @@
 import './header'
 import './header.css'
 
-function Header(){
-    return(
-        <div>
-          
-        <form action="" method="get">
-        
-            
-        </form>
-        
-        
+function Header() {
+  return (
+    <div>
 
-        <input placeholder="Фильм, событие, время">
-            
-        </input>
-        
-        
-        <a href="">
+      <form action="" method="get">
+
+
+      </form>
+
+
+
+      <input placeholder="Фильм, событие, время">
+
+      </input>
+
+
+      <a href="">
         <button type="submit"></button>
-        </a>
-        
-        <a href="">
-          <button1 type="submit" value="Кнопка"></button1> 
-        </a>
-        
-        
-        <a href="">
-          <button2 type="submit" value="Logo"></button2>
-        </a>
-        <form2 action="" method="get">
-        
-            
-        </form2>
+      </a>
 
-        <a href="">
+      
+        <button1 type="submit" value="Кнопка"></button1>
+      
+
+
+      <a href="">
+        <button2 type="submit" value="Logo"></button2>
+      </a>
+      <form2 action="" method="get">
+
+
+      </form2>
+
+      
         <button3 type="submit" value="Все"></button3>
-        </a>
-        <a href="">
+      
+      <a href="">
         <button4 type="submit" value="КИНО"></button4>
-        </a>
-        <a href="">
+      </a>
+      <a href="">
         <button5 type="submit" value="ТЕАТР"></button5>
-        </a>
-        <a href="">
+      </a>
+      <a href="">
         <button6 type="submit" value="ВЫСТАВКИ"></button6>
-        </a>
-        </div>
-    )   
+      </a>
+    </div>
+  )
 }
 export default Header;
