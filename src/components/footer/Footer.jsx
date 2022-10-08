eeedb44788ee9f409be09a4eb84ef90c605764b6
Студенческая <@rbiu.local>
2022-10-08 11:30:59 +0500

    API added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,78 @@
+function Footer() {
+    return (
+        <div className="footer__wrapper">
+            <div className="line">
+            </div>
+            <div className="footer__row">
+                <div className="footer__row-caption">
+                    События
+                </div>
+                <div className="footer__row-element">
+                    <div className="footer__row-element-caption">
+                        Кино
+                    </div>
+                    <div className="footer__row-element-item">Расписание сеансов</div>
+                    <div className="footer__row-element-item">Расписание кинотеатров</div>
+                    <div className="footer__row-element-item">Специальные предложения</div>
+                    <div className="footer__row-element-item">Фильмы, изменившие мир</div>
+                    <div className="footer__row-element-item">Выброр редакции</div>
+                </div>
+                <div className="footer__row-element">
+                    <div className="footer__row-element-caption">
+                        Кино
+                    </div>
+                    <div className="footer__row-element-item">Расписание сеансов</div>
+                    <div className="footer__row-element-item">Расписание кинотеатров</div>
+                    <div className="footer__row-element-item">Специальные предложения</div>
+                    <div className="footer__row-element-item">Фильмы, изменившие мир</div>
+                    <div className="footer__row-element-item">Выброр редакции</div>
+                </div>
+                <div className="footer__row-element">
+                    <div className="footer__row-element-caption">
+                        Кино
+                    </div>
+                    <div className="footer__row-element-item">Расписание сеансов</div>
+                    <div className="footer__row-element-item">Расписание кинотеатров</div>
+                    <div className="footer__row-element-item">Специальные предложения</div>
+                    <div className="footer__row-element-item">Фильмы, изменившие мир</div>
+                    <div className="footer__row-element-item">Выброр редакции</div>
+                </div>
+            </div>
+            <div className="footer__row">
+                <div className="footer__row-caption">
+                    События
+                </div>
+                <div className="footer__row-element">
+                    <div className="footer__row-element-caption">
+                        Кино
+                    </div>
+                    <div className="footer__row-element-item">Расписание сеансов</div>
+                    <div className="footer__row-element-item">Расписание кинотеатров</div>
+                    <div className="footer__row-element-item">Специальные предложения</div>
+                    <div className="footer__row-element-item">Фильмы, изменившие мир</div>
+                    <div className="footer__row-element-item">Выброр редакции</div>
+                </div>
+                <div className="footer__row-element">
+                    <div className="footer__row-element-caption">
+                        Кино
+                    </div>
+                    <div className="footer__row-element-item">Расписание сеансов</div>
+                    <div className="footer__row-element-item">Расписание кинотеатров</div>
+                    <div className="footer__row-element-item">Специальные предложения</div>
+                    <div className="footer__row-element-item">Фильмы, изменившие мир</div>
+                    <div className="footer__row-element-item">Выброр редакции</div>
+                </div>
+                <div className="footer__row-element">
+                    <div className="footer__row-element-caption">
+                        Кино
+                    </div>
+                    <div className="footer__row-element-item">Расписание сеансов</div>
+                    <div className="footer__row-element-item">Расписание кинотеатров</div>
+                    <div className="footer__row-element-item">Специальные предложения</div>
+                    <div className="footer__row-element-item">Фильмы, изменившие мир</div>
+                    <div className="footer__row-element-item">Выброр редакции</div>
+                </div>
+            </div>
+        </div>
+    );
+}
